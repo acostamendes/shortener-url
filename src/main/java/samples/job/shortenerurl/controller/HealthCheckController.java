3594706b04010c5c40af7757f8e807073f128afe
André Mendes Costa <andre.mendes.sp01@gmail.com>
2024-12-23 17:26:40 -0300

    up: updated the healthCheck class, implementing duration formatting such as seconds, minutes, hours and days.

--- src/main/java/samples/job/shortenerurl/controller/HealthCheckController.java
+++ src/main/java/samples/job/shortenerurl/controller/HealthCheckController.java
@@ -14,9 +14,26 @@ public class HealthCheckController {
 
     @GetMapping("/health")
     public Map<String, ?> healthCheck() {
+        Duration uptime = Duration.between(startTime, LocalDateTime.now());
+        long seconds = uptime.getSeconds();
+
+        String uptimeFormatted;
+        if (seconds < 60) {
+            uptimeFormatted = seconds + " seconds";
+        } else if (seconds < 3600) { // 60 seconds * 60 minutes
+            long minutes = seconds / 60;
+            uptimeFormatted = minutes + " minute" + (minutes > 1 ? "s" : "");
+        } else if (seconds < 86400) { // 60 seconds * 60 minutes * 24 hours
+            long hours = seconds / 3600;
+            uptimeFormatted = hours + " hour" + (hours > 1 ? "s" : "");
+        } else {
+            long days = seconds / 86400;
+            uptimeFormatted = days + " day" + (days > 1 ? "s" : "");
+        }
+
         return Map.of(
                 "status", "UP",
-                "uptime", Duration.between(startTime, LocalDateTime.now()).toMillis() + "ms"
+                "uptime", uptimeFormatted
         );
     }
 }
