7b8b7e72241818d111404aab2987f6b6a0745716
André Mendes Costa <andre.mendes.sp01@gmail.com>
2024-12-23 17:11:24 -0300

    feat: Creating the RedirectController class, to implement the following endpoint http://localhost:8080/{hash} and leaving the other endpoint running http://localhost:8080/api/v1/shortener/redirect/{hash}

---
+++ src/main/java/samples/job/shortenerurl/controller/RedirectController.java
@@ -0,0 +1,33 @@
+package samples.job.shortenerurl.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import samples.job.shortenerurl.model.Shortener;
+import samples.job.shortenerurl.service.ShortenerService;
+
+import java.io.IOException;
+
+@RestController
+public class RedirectController {
+
+    private final ShortenerService shortenerService;
+
+    public RedirectController (ShortenerService shortenerService){
+        this.shortenerService = shortenerService;
+    }
+
+    @GetMapping("/{hash}")
+    public void redirect(@PathVariable String hash, HttpServletResponse response) throws IOException {
+        Shortener shortener = shortenerService.findByHash(hash);
+        if (shortener == null){
+            response.sendError(HttpServletResponse.SC_NOT_FOUND,"Hash not found");
+            return;
+        }
+        shortener.setCount(String.valueOf(Integer.parseInt(shortener.getCount()) + 1));  // Incrementa o contador de acessos
+        shortenerService.updateShortener(shortener);  // Incrementa o contador de acessos
+        response.sendRedirect(shortener.getOriginLocation()); // Redireciona para a URL original
+
+    }
+}
--- src/main/java/samples/job/shortenerurl/controller/ShortenerController.java
+++ src/main/java/samples/job/shortenerurl/controller/ShortenerController.java
@@ -71,3 +71,4 @@ public class ShortenerController {
         return shortenerService.findAll();
     }
 }
+
