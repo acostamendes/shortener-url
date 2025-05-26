3a76eaf2fa44a78eedf05c1b3d13de415b01c9e8
acostamendes <andre.mendes.sp01@gmail.com>
2025-05-26 14:16:01 -0300

    feat: added CORS configuration to allow frontend access

---
+++ src/main/java/samples/job/shortenerurl/CorsConfig.java
@@ -0,0 +1,23 @@
+package samples.job.shortenerurl;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+   @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://172.27.25.228:3000","http://localhost:3000") // Frontend React
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(false); //Get, Post, Delete etc
+            }
+        };
+    }
+}
